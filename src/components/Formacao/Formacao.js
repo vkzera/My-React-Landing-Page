53e50a98a6a81a6dc0b1f65eef04a6ec01a5120e
Victor Almeida <victooralmeida1@gmail.com>
2019-08-11 11:27:48 -0300

    Corrigido texto formação, ajustado script async

--- src/components/Formacao/Formacao.js
+++ src/components/Formacao/Formacao.js
@@ -38,7 +38,7 @@ export class Formacao extends Component {
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
               <h3 className="mb-0">Escola SENAI "Antônio Adolpho Lobbe"</h3>
-              <div className="subheading mb-3">Aprendizarem Industrial</div>
+              <div className="subheading mb-3">Aprendizagem Industrial</div>
               <div>Mecânico de usinagem</div>
               {/* <p>GPA: 3.56</p> */}
             </div>
